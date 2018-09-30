de839433e139cbd4f00d8b4f15de75910471e309
Valentin Aitken <valentin.aitken@azdio.com>
2018-09-30 22:46:18 +0300

    Remove make files

--- scripts/kconfig/.zconf.lex.c.cmd
+++
@@ -1,1 +0,0 @@
-cmd_scripts/kconfig/zconf.lex.c := flex -oscripts/kconfig/zconf.lex.c -L /var/valio/linux/scripts/kconfig/zconf.l
--- scripts/kconfig/.zconf.tab.c.cmd
+++
@@ -1,1 +0,0 @@
-cmd_scripts/kconfig/zconf.tab.c := bison -oscripts/kconfig/zconf.tab.c -t -l /var/valio/linux/scripts/kconfig/zconf.y
